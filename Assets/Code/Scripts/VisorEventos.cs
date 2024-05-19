41d1d864b380faace9a2e2b852c88df6d2262c7e
ZeroconZ <jheredia216@gmail.com>
2024-05-20 00:29:19 +0200

    Text on Screen

--- Assets/Code/Scripts/VisorEventos.cs
+++ Assets/Code/Scripts/VisorEventos.cs
@@ -4,21 +4,63 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using TMPro;
+using System.IO;
+using UnityEditor;
 
 public class VisorEventos : MonoBehaviour
 {
 
-    public TextMeshProUGUI ScreenT;
+    public TextMeshProUGUI TextOnS;
+
+    Tofino tofino;
+    StreamReader reader;
+
+    public void newLine(string newLine)
+    {
+        TextOnS.text += newLine;
+        Canvas.ForceUpdateCanvases();
+        GetComponentInParent<ScrollRect>().verticalNormalizedPosition = 0f;
+    }
 
     void Start()
     {
+         
+        TextOnS.text = "";
 
-        ScreenT.text = "Moriré por alexelcapso";
-        
+        tofino = new Tofino();
+
+        string rutaR = Application.dataPath + "/Docs" + "/Logs" + "/logstofino.txt";
+        reader = new StreamReader(rutaR);
+
     }
 
     void Update()
     {
         
+        if(reader == null)
+        {
+
+            Debug.Log("Se han cometido errores");
+            return;
+
+        }
+
+        if(!reader.EndOfStream)
+        {
+
+            string linea = reader.ReadLine();
+            string type = tofino.MsgType(linea);
+
+            newLine(type);
+
+        }
+
+    }
+
+    void OnDestroy()
+    {
+
+        reader.Close();
+
     }
 }
